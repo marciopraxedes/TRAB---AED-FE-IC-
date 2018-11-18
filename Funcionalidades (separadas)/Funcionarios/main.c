0908a2ca015e6b52710527acff9cea486e71c800
Márcio Praxedes <marcio.praxedes@hotmail.com.br>
2018-11-18 15:05:48 -0200

    Atualização da funcionalidade funcionários

--- Funcionalidades (separadas)/Funcionarios/main.c
+++ Funcionalidades (separadas)/Funcionarios/main.c
@@ -1,8 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <locale.h>
 int main()
 {
-    printf("Hello world!\n");
+
+    setlocale(LC_ALL,"Portuguese");
+    /*Esse programa irá executar toda parte de cadastro
+    e leitura de dados de funcionários. Irá usar um banco de dados
+    funcionarios.txt na pasta raiz do programa*/
+
+
+
+    /*inicio - varíaveis do programa*/
+    int i=0, qtd;
+    char nome[100], telefone[20],cargo[100], salario[20], algo[2], codigo[5];
+    FILE *dbFunc;
+    /*fim - varíaveis do programa*/
+
+    /*abertura do programa para gravação*/
+    dbFunc = fopen("db-Funcionarios.txt","w"); /*arquivo db.txt aberto modo gravação*/
+
+
+    printf("Quantos funcionário deseja cadastrar? ");
+    scanf("%d", &qtd);
+
+    do
+    {
+
+    /*inicio-código*/
+    fputs("Código: ", dbFunc);
+    printf("Digite o código: ");
+    gets(codigo);
+    fputs(codigo, dbFunc);
+    /*inicio-código*/
+    fputc('\n',dbFunc);
+
+    /*inicio-nome*/
+    fputs("Nome do funcionário: ", dbFunc);
+    printf("Digite o nome do funcionário: ");
+    gets(nome);
+    fputs(nome, dbFunc);
+    /*fim-nome*/
+    fputc('\n',dbFunc);
+
+    /*inicio-telefone*/
+    fputs("Telefone: ", dbFunc);
+    printf("Digite o número do telefone: ");
+    gets(telefone);
+    fputs(telefone, dbFunc);
+    /*fim-telefone*/
+
+    fputc('\n',dbFunc);
+
+    /*inicio-cargo*/
+    fputs("Cargo: ", dbFunc);
+    printf("Digite o cargo: ");
+    gets(cargo);
+    fputs(cargo, dbFunc);
+    /*fim-cargo*/
+
+    fputc('\n',dbFunc);
+
+    /*inicio-salario*/
+    fputs("Salário: ", dbFunc);
+    printf("Digite o salário: ");
+    gets(salario);
+    fputs(salario, dbFunc);
+    /*fim-cargo*/
+
+    fputc('\n',dbFunc);
+    fputc('\n',dbFunc);
+
+    i++;
+    } while ( i<qtd );
     return 0;
 }
